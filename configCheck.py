0e003b17e18615c023e158215f26f39d6d6208c1
malfindor <sam.burton271@gmail.com>
2025-09-16 15:47:52 -0400

    Bug fixes

--- configCheck.py
+++ configCheck.py
@@ -5,13 +5,13 @@ import os
 bindIpPres = False
 bindPortPres = False
 if not os.path.exists('/etc/vigil.conf'):
-    print("Config file not found. Replace or ensure that the config file is located at /etc/vigil.conf")
+    print("Config file not found. Replace or ensure that the config file is located at /etc/vigil.conf", file=sys.stderr, flush=True)
     sys.exit(2)
 errors = []
 f = open('/etc/vigil.conf', 'r')
 contents = f.read()
 if len(contents) == 0:
-    print("Config file appears to be empty.")
+    print("Config file appears to be empty.", file=sys.stderr, flush=True)
     sys.exit(3)
 contents = contents.split('\n')
 lineNum = 0
@@ -19,16 +19,15 @@ for line in contents:
     lineNum = lineNum + 1
     if not len(line) == 0:
         if not line[0] == '#':
+            line = line.split('=')
             if line[0] == 'bind_ip':
                 bindIpPres = True
-                line = line.split('=')
                 if len(line[1]) == 0:
                     errors.append("Missing value for variable 'bind_ip' on line " + str(lineNum))
                 elif not ((line[1][0] == '"') and (line[1][-1] == '"')):
                     errors.append("Missing quotation marks around variable 'bind_ip' on line " + str(lineNum))
             if line[0] == 'bind_port':
                 bindIpPres = True
-                line = line.split('=')
                 if len(line[1]) == 0:
                     errors.append("Missing value for variable 'bind_port' on line " + str(lineNum))
                 elif (int(line[1]) < 1) or (int(line[1]) > 65535):
@@ -36,5 +35,5 @@ for line in contents:
 
 if not len(errors) == 0:
     for error in errors:
-        print(error)
+        print(error, file=sys.stderr, flush=True)
     sys.exit(1)
